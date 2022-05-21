b10559b5d0a153d366387196fd8ab24eb7c58c9c
victorvictoria-maker <viktohrier01@gmail.com>
2022-05-21 05:28:43 +0100

    My first push to git

---
+++ Python/Addingnumbers.py
@@ -0,0 +1,4 @@
+varA = 5;
+varB = 6;
+varC = varA + varB;
+print(varC);
---
+++ Python/countingWords.py
@@ -0,0 +1,3 @@
+varData = "Zuri is presently teaching young adults on how to learn how to code in order to solve world problems";
+varWord = varData.split();
+print(len(varWord));
